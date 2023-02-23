2643b3c74f59c0c26bf47e637e2c5a87ed6ee877
Sexure <sexurexion@gmail.com>
2023-02-23 17:44:24 +0000

    Encapsulation

---
+++ Encapsulation.java
@@ -0,0 +1,14 @@
+public class Encapsulation {
+
+        private int myField;
+      
+        public int getMyField() {
+          return myField;
+        }
+      
+        public void setMyField(int myField) {
+          this.myField = myField;
+        }
+      
+    
+}
