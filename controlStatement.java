b75d1cb86a5db8df22f28aef12b99825866384eb
Sexure <sexurexion@gmail.com>
2023-02-23 17:35:49 +0000

    Control statements

---
+++ controlStatement.java
@@ -0,0 +1,38 @@
+public class ControlStatement {
+
+    if (condition) {
+        // Code
+      } else if (condition) {
+        // Code
+      } else {
+        // Code
+      }
+      
+      switch (variable) {
+        case value1:
+          // Code
+          break;
+        case value2:
+          // Code
+          break;
+        default:
+          // Code
+      }
+      
+      for (initialization; condition; update) {
+        // Code
+      }
+      
+      while (condition) {
+        // Code
+      }
+      
+      do {
+        // Code
+      } while (condition);
+      
+      break;
+      continue;
+      
+    
+}
