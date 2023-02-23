0f4f247ecd016db3fbd3c5178edf4c3e3a6f2a7a
Sexure <sexurexion@gmail.com>
2023-02-23 17:44:58 +0000

    Polymorphism

---
+++ polymorphism.java
@@ -0,0 +1,12 @@
+public class MyClass {
+    public void myMethod() {
+      // Code
+    }
+  }
+  
+  public class MyOtherClass extends MyClass {
+    public void myMethod() {
+      // Code
+    }
+  }
+  
