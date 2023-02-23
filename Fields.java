56627b8a4fa5bb7c451756d9200b41ec24bb37d6
Sexure <sexurexion@gmail.com>
2023-02-23 17:31:33 +0000

    java fields

---
+++ Fields.java
@@ -0,0 +1,7 @@
+public class Fields {
+    private int myField;    // private access
+    public int myField;     // public access
+    static int myField;     // static access
+    final int myField;      // final (constant) access
+
+}
